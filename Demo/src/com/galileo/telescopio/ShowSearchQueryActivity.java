b6304bcf831d546a61125132c36124388cfafa43
sstevenmr <esmasx.steven266@gmail.com>
2014-01-24 13:02:43 -0600

    Demo 4

--- Demo/src/com/galileo/telescopio/ShowSearchQueryActivity.java
+++ Demo/src/com/galileo/telescopio/ShowSearchQueryActivity.java
@@ -4,9 +4,6 @@ import android.os.Bundle;
 import android.app.Activity;
 import android.content.Intent;
 import android.view.Menu;
-import android.view.View;
-import android.view.View.OnClickListener;
-import android.widget.EditText;
 import android.widget.TextView;
 
 public class ShowSearchQueryActivity extends Activity {

c290580c59f53fdbd19c321cf8cfb522f59175e3
sstevenmr <esmasx.steven266@gmail.com>
2014-03-04 11:08:33 -0600

    actualizacion tarea

---
+++ Demo3_S3/src/com/example/fragments/AbaoutFragment.java
@@ -0,0 +1,20 @@
+package com.example.fragments;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.ejemplo.R;
+
+public class AbaoutFragment extends Fragment {
+	@Override
+	public View onCreateView(LayoutInflater inflater, ViewGroup container,
+			Bundle savedInstanceState) {
+		// TODO Auto-generated method stub
+		View view = inflater.inflate(R.layout.fragment_about, container,false);
+	
+		return view;
+	}
+}
